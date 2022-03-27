ccf70a813993e49e4ab3829c7334c0fdd02aa56e
Rachel Yu <mx745796657@gmail.com>
2022-03-27 13:51:25 +1100

    Add banner with image placeholder

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,13 @@
 import React from "react";
 import Template from "./template";
+import './about.scss';
 
-const About = () => <Template />;
+const About = () => (
+  <Template>
+    <div className="banner">
+      About us
+    </div>
+  </Template>
+);
 
 export default About;
