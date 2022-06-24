e01488330d5f6e934d4278e3b879803af6d8a36d
Rachel Yu <mx745796657@gmail.com>
2022-06-24 20:36:11 +1000

    Fix dropdown render issue

--- src/components/TeamPhoto/TeamPhoto.js
+++ src/components/TeamPhoto/TeamPhoto.js
@@ -8,9 +8,9 @@ const TeamPhoto = () => {
   const [selected, setSelected] = React.useState("Executive");
   return (
     <div className="container">
-      {data.map((e, key) => {
-        <DropdownMenu portfolio={e} key={key} setSelected={setSelected} />;
-      })}
+      {data.map((e, key) => (
+        <DropdownMenu portfolio={e} key={key} setSelected={setSelected} />
+      ))}
       <PhotoDisplay selected={selected} data={data} />
     </div>
   );
