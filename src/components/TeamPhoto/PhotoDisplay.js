5d245d71b2d8f5afde4e4eb4918eb709743e8cbd
BC <btrchen112@gmail.com>
2022-06-26 14:11:06 +1000

    Work on photo display - not working

--- src/components/TeamPhoto/PhotoDisplay.js
+++ src/components/TeamPhoto/PhotoDisplay.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const PhotoDisplay = ({ selected, data }) => {
-  return <>PhotoDisplay</>;
+  let display = [];
+  const findTeam = (subcom) => {
+    for (let team in data) {
+      if (subcom in data[team]) {
+        return team;
+      }
+    }
+  };
+
+  if (!data[selected]) {
+    const team = findTeam(selected);
+    display = Object.values(data[team]).flat();
+  } else {
+    display = data[selected][selected];
+  }
+
+  return (
+    <div className="teamPhotoContainer">
+      Team Photo
+      {display.map((e, i) => {
+        return <img src={e} key={i}></img>;
+      })}
+    </div>
+  );
 };
 
 export default PhotoDisplay;
