cfb895f23bcbc11524ad24e4e5c46a6407548460
BC <btrchen112@gmail.com>
2023-05-27 16:20:30 +1000

    Add new team photos

--- src/components/Team/data.js
+++ src/components/Team/data.js
@@ -1,28 +1,17 @@
-import photo from "../../images/team/timothy.jpg";
-import president from "../../images/team/nathan.jpg";
-import vicePresident from "../../images/team/daphne.jpg";
-import softwareColead1 from "../../images/team/allan.png";
-import softwareColead2 from "../../images/team/sano.jpg";
+import artDesignLead from "../../images/team/natalie.jpg";
+import teamManager from "../../images/team/allan.png";
+import mechanicalLead from "../../images/team/daniel.jpg";
 import electricalLead from "../../images/team/aayush.jpeg";
 
-import adminManager from "../../images/team/dominique.jpeg";
-import eventsLead from "../../images/team/zami.png";
-import fSLead from "../../images/team/mohique.jpeg";
-import marketingColead1 from "../../images/team/charlotte.jpeg";
 import generic from "../../images/team/generic.png";
 
 const data = {
   Executive: {
     Executive: [
       {
-        name: "Nathan Quan",
-        role: "President",
-        img: president,
-      },
-      {
-        name: "Daphne Li",
-        role: "Vice President",
-        img: vicePresident,
+        name: "Allan Goodman",
+        role: "Team Manager",
+        img: teamManager,
       },
     ],
   },
@@ -30,74 +19,39 @@ const data = {
   Technical: {
     Technical: [
       {
+        name: "Ray Gu",
+        role: "Technical Manager",
+        img: generic,
+      },
+      {
+        name: "Mitchell Wallace",
+        role: "Software Lead",
+        img: generic,
+      },
+      {
+        name: "Tejit Pradhan",
+        role: "Web Lead",
+        img: generic,
+      },
+      {
+        name: "Daniel Cicek",
+        role: "Mechanical Lead",
+        img: mechanicalLead,
+      },
+      {
         name: "Aayush Rajouria",
         role: "Electrical Lead",
         img: electricalLead,
       },
-      {
-        name: "Allan Goodman",
-        role: "Software Colead",
-        img: softwareColead1,
-      },
-      {
-        name: "Sanojan Thiyagaraja",
-        role: "Software Colead",
-        img: softwareColead2,
-      },
-      {
-        name: "Ray Gu",
-        role: "Mechanical Lead",
-        img: generic,
-      },
     ],
-    // Mechanical: [],
-    // Electrical: [],
-    // Software: [],
-  },
-
-  Admin: {
-    Admin: [
-      {
-        name: "Dominique Djaidiguna",
-        role: "Administration Manager",
-        img: adminManager,
-      },
-      {
-        name: "Zami Lee",
-        role: "Events Lead",
-        img: eventsLead,
-      },
-      {
-        name: "Mohique Gajdhar",
-        role: "Finance and Sponsorship Lead",
-        img: fSLead,
-      },
-      {
-        name: "Charlotte Chu",
-        role: "Marketing Colead",
-        img: marketingColead1,
-      },
-      {
-        name: "Cicy Zhou",
-        role: "Marketing Colead",
-        img: generic,
-      },
-      {
-        name: "Jamie Mo",
-        role: "Media Lead",
-        img: generic,
-      },
-    ],
-    // "Finance & Sponsorship": [],
-    // Marketing: [],
   },
 
   "Art and Design": {
     "Art and Design": [
       {
-        name: "Timothy Jing",
-        role: "Art and Design Director",
-        img: photo,
+        name: "Natalie Chen",
+        role: "Art and Design Lead",
+        img: artDesignLead,
       },
     ],
   },
