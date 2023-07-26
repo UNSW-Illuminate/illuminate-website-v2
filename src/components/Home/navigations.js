05b6bd2bce7a59e7b1d9287dc5cadda8c0b86738
BC <btrchen112@gmail.com>
2023-07-26 16:11:55 +1000

    Rename navigations.js

---
+++ src/components/Home/navigations.js
@@ -0,0 +1,24 @@
+const navigations = [
+  {
+    title: "Projects",
+    link: "/projects",
+  },
+  {
+    title: "Events",
+    link: "/events",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Team",
+    link: "/team",
+  },
+  {
+    title: "Contact",
+    link: "/contact",
+  },
+];
+
+export default navigations;
