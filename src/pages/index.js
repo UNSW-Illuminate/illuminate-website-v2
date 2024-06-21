9798dfad70c48f75b7b3b00e598fbc80f3601368
Nathan-CSE <86046189+Nathan-CSE@users.noreply.github.com>
2024-06-21 18:43:21 +1000

    removed overflowing elements from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,8 @@ const IndexPage = () => {
         {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
       </head>
       <Hero />
-      <About />
+      {/* Commenting this out until fixed */}
+      {/* <About /> */}
       {isLaptop ? <Collage /> : <MobileList />}
     </Template>
   );
@@ -24,5 +25,5 @@ const IndexPage = () => {
 export default IndexPage;
 
 export function Head() {
-  return <title>Hello World</title>;
+  return <title>UNSW Illuminate</title>;
 }
