85f6da0a55c8aa74dba678288a3bdd2773859581
BC <btrchen112@gmail.com>
2024-07-20 12:54:51 +1000

    Add people and department data

---
+++ src/components/Team/department_data.js
@@ -0,0 +1,33 @@
+import software from "../../images/team/departments/software_department.png";
+import mechanical from "../../images/team/departments/mechanical_department.jpg";
+import electrical from "../../images/team/departments/electrical_department.jpg";
+import art from "../../images/team/departments/art_design_department.jpg";
+
+const departments = [
+  {
+    name: "Software",
+    img: software,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac massa  vel ante suscipit facilisis. Integer vel imperdiet quam.  Integer vel imperdiet quam.",
+  },
+  {
+    name: "Mechanical",
+    img: mechanical,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac massa  vel ante suscipit facilisis. Integer vel imperdiet quam.  Integer vel imperdiet quam.",
+  },
+  {
+    name: "Electrical",
+    img: electrical,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac massa  vel ante suscipit facilisis. Integer vel imperdiet quam.  Integer vel imperdiet quam.",
+  },
+  {
+    name: "Art and Design",
+    img: art,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac massa  vel ante suscipit facilisis. Integer vel imperdiet quam.  Integer vel imperdiet quam.",
+  },
+];
+
+export default departments;
---
+++ src/components/Team/people_data.js
@@ -0,0 +1,40 @@
+import generic from "../../images/team/generic.png";
+
+const people = {
+  team: [
+    {
+      name: "Elsa Doan",
+      role: "Team Lead",
+      img: generic,
+    },
+    {
+      name: "Jonathan Cao",
+      role: "Team Lead",
+      img: generic,
+    },
+  ],
+  technical: [
+    {
+      name: "Nathan Nguyen",
+      role: "Software Lead",
+      img: generic,
+    },
+    {
+      name: "Yangyue Jiang",
+      role: "Mechanical Lead",
+      img: generic,
+    },
+    {
+      name: "Andre Monteiro",
+      role: "Electrical Lead",
+      img: generic,
+    },
+    {
+      name: "Sanjna Raisinghani",
+      role: "Art & Design Lead",
+      img: generic,
+    },
+  ],
+};
+
+export default people;
